c7e731ddc9d5bf0280c9051e194f40e29735c338
unknown <berunadotte@gmail.com>
2024-11-10 23:18:32 +0300

    add responsive design

--- src/App.vue
+++ src/App.vue
@@ -2,53 +2,91 @@
   <div class="bg-white min-h-screen flex flex-col items-center font-sans">
     <header class="text-center mt-[48px]">
       <img src="./assets/images/logo.svg" alt="Logo" class="mx-auto" />
-      <nav class="text-gray-500 text-[13px] space-x-2.5 pt-[22px] max-sm:mt-[4px]">
-        <span>Хлебные крошки</span> →
-        <span>Хлебные крошки</span> →
+      <nav
+        class="text-gray-500 text-[13px] space-x-2.5 pt-[22px] max-sm:mt-[4px] max-sm:space-x-2"
+      >
+        <span v-if="!isSmallScreen">Хлебные крошки</span>
+        <span v-else>Главная</span> →
+        <span v-if="!isSmallScreen">Хлебные крошки</span>
+        <span v-else>Рассылки</span> →
         <span>Хлебные крошки</span>
       </nav>
-      <h1 class="text-[56px]  font-medium mt-[17px] max-sm:text-[32px] max-sm:mt-[4px] ">Подписки «Клерка»</h1>
+      <h1
+        class="text-[56px] font-medium mt-[17px] max-sm:text-[32px] max-sm:mt-[4px]"
+      >
+        Подписки «Клерка»
+      </h1>
     </header>
 
     <div class="mt-[18px] max-sm:mt-[20px]">
-      <ul class="flex space-x-6 text-[16px] tracking-tighter font-medium text-blue-500 max-sm:space-x-7">
-        <li class="border-solid border-b-2 border-blue-500 pb-[16px]"><a href="#">Рассылки</a></li>
-        <li class="border-solid hover:border-b-2 border-blue-500 pb-[16px] text-gray-400 hover:text-blue-500"><a href="#">Соцсети</a></li>
-        <li><a href="#" class="border-solid hover:border-b-2 border-blue-500 pb-[19px] text-gray-400 hover:text-blue-500">Мессенджеры</a></li>
+      <ul
+        class="flex space-x-6 text-[16px] tracking-tighter font-medium text-blue-500 max-sm:space-x-8 max-sm:ml-[9px]"
+      >
+        <li class="border-solid border-b-2 border-blue-500 pb-[16px]">
+          <a href="#">Рассылки</a>
+        </li>
+        <li
+          class="border-solid hover:border-b-2 border-blue-500 pb-[16px] text-gray-400 hover:text-blue-500"
+        >
+          <a href="#">Соцсети</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="border-solid hover:border-b-2 border-blue-500 pb-[19px] text-gray-400 hover:text-blue-500"
+            >Мессенджеры</a
+          >
+        </li>
       </ul>
     </div>
 
-    <section class="bg-bg-gray rounded-lg shadow-lg w-full max-w-[1024px]">
-      <h2 class="text-center text-[24px] font-medium mb-[21px] mt-[46px] max-sm:mx-[16px] max-sm:text-left max-sm:mt-[31px] max-sm:leading-[32px]">
+    <section
+      class="bg-bg-gray rounded-lg shadow-lg w-full max-w-[1024px] max-sm:max-w-full"
+    >
+      <h2
+        class="text-center text-[24px] font-medium mb-[21px] mt-[46px] max-sm:mx-[8px] max-sm:text-left max-sm:mt-[31px] max-sm:leading-[32px] max-sm:mb-[16px]"
+      >
         Выберите рассылки, которые подходят именно вам
       </h2>
 
-      <div class="
-       flex 
-       items-center
-       justify-between 
-       mb-[14px]
-       ml-[45px]
-       mr-[47px] 
-       bg-white
-       rounded-l-full 
-       rounded-r-full">
+      <div
+        class="flex items-center justify-between mb-[14px] ml-[45px] mr-[47px] bg-white rounded-l-full rounded-r-full max-sm:mb-[18px] max-sm:ml-[14px] max-sm:mr-[8px]"
+      >
         <input
           type="email"
           placeholder="Электронная почта"
-          class="p-[18px] rounded-l-lg"
+          class="p-[18px] rounded-l-lg max-sm:p-[16px] max-sm:pl-[10px]"
         />
-        <button type="button" class="h-[58px] w-[164px] text-white bg-blue-500 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full px-6 py-3 text-center">{{ isSmallScreen ? '>' : Подписаться}}</button>
+        <button
+          type="button"
+          class="h-[58px] w-[164px] text-white bg-blue-500 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full px-6 py-3 text-center max-sm:h-[56px] max-sm:w-[56px] max-sm:p-0 max-sm:pl-[16px]"
+        >
+          <img
+            v-if="isSmallScreen"
+            src="./assets/images/arrow.svg"
+            alt="Arrow"
+            class="w-[24px] h-[24px]"
+          />
+          <p v-else>Подписаться</p>
+        </button>
       </div>
-      <div class="flex items-center mb-[32px]">
-        <label class="ml-[48px] inline-flex items-center cursor-pointer">
-          <input type="checkbox" value="" class="sr-only peer">
-          <div class="relative w-[32px] h-[16px] bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[1px] after:start-[1px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[14px] after:w-[14px] after:transition-all peer-checked:bg-blue-600"></div>
-          <span class="ml-[8px] text-sm font-{400} text-gray-900">Подписаться на все рассылки</span>
+      <div class="flex items-center mb-[32px] max-sm:mb-[22px]">
+        <label
+          class="ml-[48px] inline-flex items-center cursor-pointer max-sm:ml-[8px]"
+        >
+          <input type="checkbox" value="" class="sr-only peer" />
+          <div
+            class="relative w-[32px] h-[16px] bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[1px] after:start-[1px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[14px] after:w-[14px] after:transition-all peer-checked:bg-blue-600"
+          ></div>
+          <span class="ml-[8px] text-sm font-{400} text-gray-900"
+            >Подписаться на все рассылки</span
+          >
         </label>
       </div>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-x-[0px] gap-y-[25px] justify-items-center mb-[46px] px-[14px]">
+      <div
+        class="grid grid-cols-2 max-sm:grid-cols-1 gap-x-[0px] gap-y-[25px] justify-items-center mb-[46px] px-[14px] max-sm:gap-y-[24px]"
+      >
         <SubscriptionCard
           comment="Перед рассветом"
           title="Утренний бухгалтер"
@@ -60,7 +98,7 @@
         <SubscriptionCard
           comment="После заката"
           title="Ночной бухгалтер"
-          description="Самая краткая газета о налогах и бухучете в мире – современная рассылка для чтения."
+          description="Самая краткая газета о налогах и бухучете в мире — современная рассылка для чтения."
           features="Анализ, оценка и только самое главное;Лучшие комменты юзеров в обзоре"
           subscribers="37 480 человек"
           icon="../src/assets/images/frame3507.png"
@@ -84,14 +122,14 @@
       </div>
     </section>
 
-    <footer class="mt-[48px] text-5xl text-center">
+    <footer class="mt-[48px] mb-[48px] text-5xl text-center max-sm:mt-[32px]">
       ¯\_(ツ)_/¯
     </footer>
   </div>
 </template>
 
 <script>
-import SubscriptionCard from "./components/SubscriptionCard.vue";
+import SubscriptionCard from './components/SubscriptionCard.vue'
 
 export default {
   components: {
@@ -100,22 +138,21 @@ export default {
   data() {
     return {
       isSmallScreen: false,
-    };
+    }
   },
   mounted() {
-    this.checkScreenSize();
-    window.addEventListener('resize', this.checkScreenSize);
+    this.checkScreenSize()
+    window.addEventListener('resize', this.checkScreenSize)
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.checkScreenSize);
+    window.removeEventListener('resize', this.checkScreenSize)
   },
   methods: {
     checkScreenSize() {
-      this.isSmallScreen = window.innerWidth <= 640; // Tailwind's sm breakpoint is 640px
+      this.isSmallScreen = window.innerWidth <= 640 // Tailwind's sm breakpoint is 640px
     },
   },
-};
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/SubscriptionCard.vue
+++ src/components/SubscriptionCard.vue
@@ -1,16 +1,25 @@
 <template>
   <div
-    class="flex items-start border rounded-2xl shadow-md bg-gray-50 w-[478px] pt-[22px] pl-[24px]"
+    class="flex items-start border rounded-2xl shadow-md bg-gray-50 w-[478px] max-h-[244px] pt-[22px] pl-[24px] max-sm:w-[343px] max-sm:pt-[22px] max-sm:pl-[16px] max-sm:max-h-[272px]"
   >
-    <div>
+    <div class="relative">
       <p class="text-[13px] text-gray-400 font-[400]">{{ comment }}</p>
       <h3 class="text-[20px] font-semibold leading-[16px] mt-[4px]">
         {{ title }}
       </h3>
-      <p class="text-gray-600 text-[14px] mt-[13px] max-w-[320px]">
+      <img
+        :src="icon"
+        alt=""
+        class="absolute top-[-1px] right-[-124px] w-[80px] h-[80px] mr-[16px] ml-[28px] mt-[2px] max-sm:w-[40px] max-sm:h-[40px] max-sm:pt-[0px] max-sm:top-[0px] max-sm:right-[-1px]"
+      />
+      <p
+        class="text-gray-600 text-[14px] mt-[13px] max-w-[320px] max-sm:max-w-full max-sm:mr-[20px]"
+      >
         {{ description }}
       </p>
-      <ul class="text-sm text-black space-y-1">
+      <ul
+        class="text-sm text-black space-y-1 max-sm:max-w-[310px] max-sm:pb-[3px]"
+      >
         <li
           v-for="(feature, index) in featuresArray"
           :key="index"
@@ -19,13 +28,13 @@
         >
           <img
             src="../assets/images/checkmark.svg"
-            class="w-[16px] h-[16px] mr-2"
+            class="w-[16px] h-[16px] mr-[8px]"
             alt="checkmark"
           />
           {{ feature }}
         </li>
       </ul>
-      <div class="flex items-center mt-4">
+      <div class="flex items-center mt-[16px] max-sm:mt-[22px]">
         <label class="inline-flex items-center cursor-pointer mb-[22px]">
           <input
             type="checkbox"
@@ -35,13 +44,13 @@
           <div
             class="relative w-[48px] h-[24px] bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[1px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[22px] after:w-[22px] after:transition-all peer-checked:bg-blue-600"
           ></div>
-          <span class="ml-[8px] text-sm font-medium text-gray-900"
+          <span
+            class="ml-[8px] text-sm font-medium text-gray-900 max-sm:ml-[16px]"
             >Уже получает {{ subscribers }}</span
           >
         </label>
       </div>
     </div>
-    <img :src="icon" alt="" class="w-[80px] h-[80px] mr-4 ml-[28px] mt-[2px]" />
   </div>
 </template>
 
